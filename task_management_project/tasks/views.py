ae78a3adb2456a7ab52756e50a83e28858a51e29
LKFCruz <lr.lkcruz@mmdc.mcl.edu.ph>
2025-01-25 22:09:33 +0800

    Fixed CSRF issue in create_user view and added JSON parsing

--- task_management_project/tasks/views.py
+++ task_management_project/tasks/views.py
@@ -1,18 +1,22 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import User
 from django.db import IntegrityError
 
 # Create User
+@csrf_exempt
 def create_user(request):
     if request.method == 'POST':
         try:
             # Extract user data from the request
-            username = request.data['username']
-            email = request.data['email']
+            import json
+            data = json.loads(request.body)  # Parse raw JSON body
+            username = data['username']
+            email = data['email']
 
             # Create a new user
             user = User.objects.create(username=username, email=email)
-            
+
             # Return a success response
             return JsonResponse({'id': user.id, 'message': 'User created successfully'}, status=201)
 
@@ -30,4 +34,3 @@ def get_users(request):
         users = User.objects.all()  # Fetch all users
         user_list = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
         return JsonResponse({'users': user_list}, status=200)
-    
