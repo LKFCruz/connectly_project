e2d7523e7d6e7a12c2f1bbb550be5290fa84470f
LKFCruz <lr.lkcruz@mmdc.mcl.edu.ph>
2025-01-26 14:43:08 +0800

    Remove task_management_project files from source control

--- task_management_project/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.get_users, name='get_users'),  # For GET request to fetch all users
-    path('users/create/', views.create_user, name='create_user'),  # For POST request to create a user
-    
-]
